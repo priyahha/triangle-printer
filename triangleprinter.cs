47370027eeb498b50d8ca7361f69d6a14dc76f34
priyahha <108473363+priyahha@users.noreply.github.com>
2023-06-30 12:41:56 +0530

    Update triangleprinter.cs

--- triangleprinter.cs
+++ triangleprinter.cs
@@ -1,1 +1,20 @@
+using System;
 
+public class TrianglePrinter
+{
+    public static void PrintTriangle(int height)
+    {
+        for (int i = 1; i <= height; i++)
+        {
+            Console.WriteLine(new string('*', i));
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the height of the triangle: ");
+        int height = int.Parse(Console.ReadLine());
+
+        PrintTriangle(height);
+    }
+}
